74ea6aa0c2e4a5a3bbb5efe701e7828e5286cb3d
damian <damian.szalimow@onet.eu>
2023-01-15 10:07:47 +0100

    add input htm

---
+++ input.js
@@ -0,0 +1,26 @@
+const add = document.getElementById("form")
+
+function send() {
+    var itemTitle = document.getElementById("item-title").value;
+    var itemBarcode = document.getElementById("item-barcode").value;
+    let itemDescription = document.getElementById("item-description".value);
+
+    const json = {
+        itemTitle: itemTitle,
+        itemBarcode: itemBarcode,
+    };
+    
+    fetch("http://localhost:3000/orders", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+        });
+        }
+
+add.addEventListener("submit", e=>{
+    e.preventDefault();
+    send();
+})
+console.log(add)

219ed0f77d0e2dfdfc65044cc5b120ca9ecb8222
damian <damian.szalimow@onet.eu>
2023-01-18 23:00:53 +0100

    .

--- input.js
+++ input.js
@@ -1,4 +1,19 @@
+var uniqid = require('./node_modules/uniqid/index'); 
+
+console.log(uniqid()); // -> 4n5pxq24kpiob12og9
+console.log(uniqid(), uniqid());
+
+
+
+
+
+
+
+
+
+
 const add = document.getElementById("form")
+const addReceive = document.getElementById("receive-form")
 
 function send() {
     var itemTitle = document.getElementById("item-title").value;
@@ -19,9 +34,18 @@ function send() {
         });
         }
 
+function sendReceive(){
+    console.log("wysłano nowy item")
+}
+
+
 add.addEventListener("submit", e=>{
     e.preventDefault();
     send();
-    console.log("dupa")
 })
-console.log(add)
+console.log(addReceive)
+
+addReceive.addEventListener("submit", e=>{
+    e.preventDefault();
+    sendReceive();
+})

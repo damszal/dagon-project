8c478eb64cd39a5b1ccd231167448d5a2944462f
damian <damian.szalimow@onet.eu>
2023-01-24 18:46:41 +0100

    refactor panel.js

--- pick/panel.js
+++ pick/panel.js
@@ -9,82 +9,103 @@ const iCode = document.querySelector('.item-code')
 const iProperty = document.querySelector('.item-property')
 const img = document.querySelector(".i-img")
 
-
-console.log(qToPick)
-console.log(iDescription)
-console.log(iCode)
-console.log(iProperty)
-console.log(img.src)
-
 logOut.addEventListener('click', (e)=>{
     localStorage.removeItem("item")
     window.location.replace("/logouted.htm");
 
 })
 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 window.addEventListener('DOMContentLoaded', (e) => {
-    qToPick.innerHTML = order1.toPick;
-    iDescription.innerHTML = order1.description;
-    iCode.innerHTML = order1.code;
-    iProperty.innerHTML = order1.property
+    fetch("http://localhost:3000/topick")
+        .then(res => res.json())
+        .then(res => {
+            qToPick.innerHTML = 3;
+            iCode.innerHTML = res[0].itemName;
+            iDescription.innerHTML = res[0].itemCategory;
+            iProperty.innerHTML = res[0].itemDescription;
+            
+            // --------------------------------------- pick
+            
+            let counter = 0; 
+
+            scanBtn.addEventListener('click', (e)=> {
+                e.preventDefault;
+                picked.innerHTML++
+                if (picked.innerHTML == qToPick.innerHTML) {
+                    picked.innerHTML = 0 
+                    counter++
+                    iDescription.innerHTML = res[counter].itemCategory;
+                    iCode.innerHTML = res[counter].itemName;
+                    iProperty.innerHTML = res[counter].itemDescription
+                    qToPick.innerHTML = 2;
+                    //qToPick.innerHTML = res[counter].toPick;
+                    // img.src = res[counter].img
+                }
+            })
+        })
+
 })
 
 // ***************************************************************************
 
-const order1 = {
-    toPick : 5,
-    description : "mata fitness",
-    code : "TPE 0,6cm",
-    property : "jasnoniebieska",
-    img: "assets/tpe0-6.jpg",
-};
+// const order1 = {
+//     toPick : 5,
+//     description : "mata fitness",
+//     code : "TPE 0,6cm",
+//     property : "jasnoniebieska",
+//     img: "assets/tpe0-6.jpg",
+// };
 
-const order2 = {
-    toPick : 3,
-    description : "mata fitness",
-    code : "NBR 1,5cm",
-    property : "żółta",
-    img: "assets/nbr1-5.jpg",
+// const order2 = {
+//     toPick : 3,
+//     description : "mata fitness",
+//     code : "NBR 1,5cm",
+//     property : "żółta",
+//     img: "assets/nbr1-5.jpg",
     
-}
+// }
 
-const order3 = {
-    toPick : 3,
-    description : "mata fitness",
-    code : "NBR 1,5cm",
-    property : "z otworami czarna",
-    img: "assets/nbr1-5z-otworami.jpg",
+// const order3 = {
+//     toPick : 3,
+//     description : "mata fitness",
+//     code : "NBR 1,5cm",
+//     property : "z otworami czarna",
+//     img: "assets/nbr1-5z-otworami.jpg",
 
-}
-const orders = [order1,order2,order3]
+// }
+// const orders = [order1,order2,order3]
 
-let counter = 0; 
+// let counter = 0; 
 
-scanBtn.addEventListener('click', (e)=> {
-    e.preventDefault;
-    picked.innerHTML++
-    if (picked.innerHTML == qToPick.innerHTML) {
-        picked.innerHTML = 0 
-        iDescription.innerHTML = orders[counter++].description;
-        iCode.innerHTML = orders[counter].code;
-        iProperty.innerHTML = orders[counter].property
-        qToPick.innerHTML = orders[counter].toPick;
-        img.src = orders[counter].img
-    }
-})
+// scanBtn.addEventListener('click', (e)=> {
+//     e.preventDefault;
+//     picked.innerHTML++
+//     if (picked.innerHTML == qToPick.innerHTML) {
+//         picked.innerHTML = 0 
+//         iDescription.innerHTML = orders[counter++].description;
+//         iCode.innerHTML = orders[counter].code;
+//         iProperty.innerHTML = orders[counter].property
+//         qToPick.innerHTML = orders[counter].toPick;
+//         img.src = orders[counter].img
+//     }
+// })
 
 //*********************************************************************
 // TEMPORARY SOUND EFFECT
 
 function playError(){
-    let audio = new Audio("audio/error.wav");
+    let audio = new Audio("../audio/error.wav");
     audio.play()
 }
 
 errBtn.addEventListener('click',playError);
 
 function playPick(){
-    let audio = new Audio("audio/pick.wav");
+    let audio = new Audio("../audio/pick.wav");
     audio.play()
 }
 
